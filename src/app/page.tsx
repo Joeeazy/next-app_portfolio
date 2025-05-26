a40e74852ffc0555cfd676c8dac92a793b1ce326
Joeeazy <joeeazy34@gmail.com>
2025-05-26 14:18:29 +0300

    Projects section done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import Blogs from "./components/Blogs";
 import Hero from "./components/Hero";
+import Projects from "./components/Projects";
 
 export default function Home() {
   return (
     <>
     <Hero />
+    <Projects />
+    <Blogs />
     </>
   );
 }
